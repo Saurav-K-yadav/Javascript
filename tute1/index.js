27c0c4ab4534719905a5b47125374ae1e424c036
Saurav Kumar yadav <sauravkumaryadav100@gmail.com>
2021-08-08 11:04:41 +0545

    Basics of Javascript

---
+++ tute1/index.js
@@ -0,0 +1,29 @@
+console.log("Test");
+// var,let,const can be used to create variable
+var name1 = "saurav";
+console.log(name1);
+var channel;
+channel = "Saurav's page";
+console.log(channel);
+var num = 100;
+var num2 = 200;
+console.log(num + num2);
+let a = 0;
+let b = 10;
+console.log(a / b);
+const c=30;
+console.log(c);
+// var is global but let is local so local is preferred
+const arr=[2,3,4];
+arr.push(8);
+console.log(arr);
+arr.pop();
+console.log(arr);
+// push and pop is allowed in const array 
+
+// DATA TYPES 
+// primitiive DAtatypes 
+let nameS="SAURAV";
+console.log("name os string "+nameS);
+console.log("Datatype is "+(typeof nameS));
+
