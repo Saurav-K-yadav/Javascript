17141f96a2f0393150eec893a9419b1bc1191a6d
Saurav Kumar yadav <sauravkumaryadav100@gmail.com>
2021-08-11 00:02:12 +0545

    Website manupulation using JS window object

---
+++ Javascript/website.js
@@ -0,0 +1,25 @@
+let a = window;
+// window.alert("Welcome");
+// dispays alert
+// a=prompt("This will destroy your PC");
+// creates a prompt and stores it value in variable
+// b=confirm("you sure?");
+console.log(a);
+b = window.innerWidth;
+console.log(b);
+b = window.innerHeight;
+console.log(b);
+b = window.outerWidth;
+console.log(b);
+b = window.outerHeight;
+console.log(b);
+b = scrollX;
+console.log(b);
+//how much window is scrolled in X directio currently
+a = location;
+console.log(a);
+a = window.history;
+console.log(a);
+//displays the history of websites visited
+// history.go(-1);
+// goes to last history
