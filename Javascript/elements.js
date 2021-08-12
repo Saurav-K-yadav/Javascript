fabd6eea739c8f7bd10cd438c0c6ee45772a4b69
Saurav Kumar yadav <sauravkumaryadav100@gmail.com>
2021-08-13 00:59:10 +0545

    Creating, Removing & Replacing Elements

--- Javascript/elements.js
+++ Javascript/elements.js
@@ -4,18 +4,26 @@ console.log(elements);
 //Adding class name to the element
 elements.className = "childul";
 elements.id = "createdli";
-elements.setAttribute('title',"mytitle");
-elements.innerText="This is appended from javascript";
+elements.setAttribute("title", "mytitle");
+elements.innerText = "This is appended from javascript";
 console.log(elements);
-let ul=document.querySelector("ul.this");
+let ul = document.querySelector("ul.this");
 console.log(ul);
 ul.appendChild(elements);
 //appends elements under ul
-let text=document.createTextNode("This is a text node");
+let text = document.createTextNode("This is a text node");
 text.className = "childul";
 elements.appendChild(text);
 
-let elem2=document.createElement("h3");
-elem2.id="elem2";
-elem2.className="elem2";
-let tnode=document.createTextNode("This is a node for elem2");
+let elem2 = document.createElement("h3");
+elem2.id = "elem2";
+elem2.className = "elem2";
+let tnode = document.createTextNode("This is a node for elem2");
+elem2.appendChild(tnode);
+elements.replaceWith(elem2);
+let myul = document.getElementById("myul");
+myul.replaceChild(elem2, document.getElementById("fui"));
+myul.removeChild(document.getElementById("elem2"));
+console.log(elem2);
+let pr = elem2.getElementsByClassName;
+console.log(elem2,pr);
