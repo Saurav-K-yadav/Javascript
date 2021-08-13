e3f87169a8978e48c14822143f8327d3b733ee9a
Saurav Kumar yadav <sauravkumaryadav100@gmail.com>
2021-08-13 13:40:12 +0545

    practised a question on append

--- Javascript/elements.js
+++ Javascript/elements.js
@@ -25,5 +25,15 @@ let myul = document.getElementById("myul");
 myul.replaceChild(elem2, document.getElementById("fui"));
 myul.removeChild(document.getElementById("elem2"));
 console.log(elem2);
-let pr = elem2.getElementsByClassName;
+let pr = elem2.getAttribute("id");
+console.log(elem2, pr);
+elem2.removeAttribute("id");
 console.log(elem2,pr);
+
+// Practise
+let a =document.createElement("h1");
+a.className="class";
+a.innerHTML=`<a href="https://www.google.com">google</a>`;
+console.log(a);
+let ap=document.querySelector(".container");
+ap.appendChild(a);
