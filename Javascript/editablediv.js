9e4acf2a84e5d6db265f29f014022dd3ce11a3b8
Saurav Kumar yadav <sauravkumaryadav100@gmail.com>
2021-08-15 23:30:13 +0545

    added event listner

--- Javascript/editablediv.js
+++ Javascript/editablediv.js
@@ -11,7 +11,9 @@ console.log(div);
 head = document.querySelector("h1");
 console.log(head);
 head.appendChild(div);
-div.addEventListener("click", func);
-function func(element) {
-  console.log(element);
-}
+
+div.addEventListener("click", function(){
+  let html=`<textarea class="form-control" class="textarea" id="textarea" rows="3"></textarea> `;
+  div.innerHTML=html;
+  
+})
