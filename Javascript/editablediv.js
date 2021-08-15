a8dde4b6e8974f8c59d80c0029d3e4d4ab7c7da4
Saurav Kumar yadav <sauravkumaryadav100@gmail.com>
2021-08-15 23:32:30 +0545

    finished the set

--- Javascript/editablediv.js
+++ Javascript/editablediv.js
@@ -2,18 +2,41 @@ console.log("Editable div");
 //You have to create a div and inject it into page which contains the heading
 //whenever someone clicks on the div, it should be converted into an editable item.
 //whenever user clicks away save the note into the local storage
-let div = document.createElement("div");
-div.className = "division";
-div.id = "div";
-div.innerHTML =
-  "<br><div>This is a division created with the help of javascript</div>";
-console.log(div);
-head = document.querySelector("h1");
-console.log(head);
-head.appendChild(div);
+let divElem = document.createElement('div');
 
-div.addEventListener("click", function(){
-  let html=`<textarea class="form-control" class="textarea" id="textarea" rows="3"></textarea> `;
-  div.innerHTML=html;
-  
-})
+let val = localStorage.getItem('text');
+let text;
+if (val==null){
+ text = document.createTextNode('This is my element. click to edit it');
+}
+else{
+    text = document.createTextNode(val);
+}
+divElem.appendChild(text);
+
+divElem.setAttribute('id', 'elem');
+divElem.setAttribute('class', 'elem');
+divElem.setAttribute('style', 'border:2px solid black; width: 154px; margin: 34px; padding:23px;');
+
+let container = document.querySelector('.container');
+let first = document.getElementById('myfirst');
+
+
+container.insertBefore(divElem, first);
+
+console.log(divElem, container, first)
+
+divElem.addEventListener('click', function () {
+    let noTextAreas = document.getElementsByClassName('textarea').length;
+    if(noTextAreas ==0){
+    let html = elem.innerHTML;
+    divElem.innerHTML = ` <textarea class="textarea form-control" id="textarea" rows="3"> ${html}</textarea>`;
+    }
+    let textarea = document.getElementById('textarea');
+    textarea.addEventListener('blur', function() {
+        elem.innerHTML = textarea.value;
+        localStorage.setItem('text', textarea.value);
+    })
+});
+
+ 
