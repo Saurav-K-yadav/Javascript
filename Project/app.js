7b3d4a31e8480500e8fb1318be55e1777c2a0a7d
Saurav Kumar yadav <sauravkumaryadav100@gmail.com>
2021-08-16 23:47:54 +0545

    made frontend of the website

--- Project/app.js
+++ Project/app.js
@@ -1,1 +1,18 @@
 console.log("Welcome to notes app");
+
+//if user adds a note, t is to be added to local storage
+let addBtn=document.getElementById("addbtn");
+addBtn.addEventListener("click",function(e){
+    let addTxt=document.getElementById("addtxt");
+let notes=localStorage.getItem("notes");
+if(notes==null){
+    notesObj=[];
+}
+else{
+    notesObj=JSON.parse(notes);
+}
+notesObj.push(addTxt.value);
+localStorage.setItem("notes",JSON.stringify(notes));
+addTxt.value="";
+console.log(notesObj);
+})
