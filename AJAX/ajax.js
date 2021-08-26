48c110d7627f65c1e46e904cac55224a70c4c7dc
Saurav Kumar yadav <sauravkumaryadav100@gmail.com>
2021-08-26 23:18:57 +0545

    created basic framework of ajax

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -1,1 +1,27 @@
 console.log("Started AJAX");
+
+let fetchBtn = document.getElementById("fetchBtn");
+fetchBtn.addEventListener("click", buttonclickHandler);
+function buttonclickHandler() {
+    console.log("Inside the event listener");
+
+    //initiating a xhr object
+    const xhr = new XMLHttpRequest();
+
+    //open the object
+    xhr.open('GET', "saurav.txt", true);
+
+    //what to do on progress
+    xhr.onprogress = function () {
+        console.log("Loading...");
+
+    }
+
+    //When the response is ready
+    xhr.onload = function () {
+        console.log(this.responseText);
+    }
+
+    //send the request
+    xhr.send();
+}
