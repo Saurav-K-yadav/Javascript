18ef590e42e16b4011d2e5ea42dcf1a8ce2224bf
Saurav Kumar yadav <sauravkumaryadav100@gmail.com>
2021-08-28 23:45:57 +0545

    learned get and post request in xml

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -11,14 +11,94 @@ function buttonclickHandler() {
     //open the object
     xhr.open('GET', "saurav.txt", true);
 
+    xhr.onreadystatechange = function () {
+        console.log("Ready state is", xhr.readyState);
+    }
     //what to do on progress
     xhr.onprogress = function () {
-        console.log("Loading...");
+        if (this.status === 200) {
+            //this checks if it is http 200 status i.e. sucessful connection is established
+
+            console.log("Loading...");
+        }
+        else {
+            //else display error message
+            alert("Error!");
+            console.log("Error!");
+        }
 
     }
 
     //When the response is ready
     xhr.onload = function () {
+        console.log(this.responseText);
+    }
+
+    //send the request
+    xhr.send();
+}
+
+//for post request
+let postbtn = document.getElementById("postBtn");
+postbtn.addEventListener("click", clickHandler);
+function clickHandler() {
+    let post = new XMLHttpRequest();
+    //the url below is from dummy sample rest api website(dummy website)
+    post.open("POST", "http://dummy.restapiexample.com/api/v1/create", true);
+    post.getResponseHeader("Content-type", "application/x-www-form-urlencoded");
+    post.onprogress = function () {
+        if (this.status === 200) {
+            console.log("Loading...")
+        }
+        else {
+            console.log("ErroR !!!");
+        }
+    }
+    post.onload = function () {
+        console.log(this.response);
+    }
+    params = "status=success&name=test&salary=123&age=23&id=25";
+    post.send(params);
+
+}
+
+//fetch Button
+let populateBtn = document.getElementById("populateBtn");
+populateBtn.addEventListener("click", Populator);
+function Populator() {
+    console.log("Clicked the populate Button");
+
+    //initiating a xhr object
+    const xhr = new XMLHttpRequest();
+
+    //open the object
+    xhr.open('GET', "sk.txt", true);
+
+    //what to do on progress
+    xhr.onprogress = function () {
+        if (this.status === 200) {
+            //this checks if it is http 200 status i.e. sucessful connection is established
+
+            console.log("Loading...");
+        }
+        else {
+            //else display error message
+            alert("Error!");
+            console.log("Error!");
+        }
+
+    }
+
+    //When the response is ready
+    xhr.onload = function () {
+        let obj = JSON.parse(this.responseText);
+        console.log(obj);
+        let list = document.getElementById("list");
+        str = "";
+        for (key in obj) {
+            str += `<li> ${obj[key]} </li>`;
+        }
+        list.innerHTML = str;
         console.log(this.responseText);
     }
 
