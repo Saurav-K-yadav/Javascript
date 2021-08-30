cb97d60a1e08d93c9869a4ac6e3f46174735b88b
Saurav Kumar yadav <sauravkumaryadav100@gmail.com>
2021-08-30 17:04:59 +0545

    The project is completed

--- Online Library/script.js
+++ Online Library/script.js
@@ -8,15 +8,49 @@ function Book(bname, author, type) {
 }
 
 //Display Constructor
-function display() {
+function Display() {
 
 }
 
 
-display.prototype.add = function () { }
+Display.prototype.add = function (book) {
+    console.log("adding to UI");
+    tableBody = document.getElementById("tableBody");
+    let = uiString = `<tr>
+                    <td>${book.bname}</td>
+                    <td>${book.author}</td>
+                    <td>${book.type}</td>
+                </tr>`;
+    tableBody.innerHTML += uiString;
+}
 
-display.prototype.clear = function () { }
+Display.prototype.clear = function () {
+    let libraryForm = document.getElementById("libraryForm");
+    libraryForm.reset();
+}
 
+Display.prototype.validate = function (book) {
+    if (book.bname.length < 2 || book.author.length < 2) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+Display.prototype.show = function (type, displayMessage) {
+    let message = document.getElementById('msg');
+    message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                            <strong>Messge:</strong> ${displayMessage}
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                            </button>
+                        </div>`;
+    setTimeout(function () {
+        message.innerHTML = ''
+    }, 2000);
+
+}
 
 
 
@@ -48,8 +82,14 @@ function libraryFormSubmitted(a) {
 
     let book = new Book(bname, author, type);
 
-    let display = new display();
-    display.add(book);
-    display.clear();
-    console.log(book);
+    let display = new Display();
+    if (display.validate(book)) {
+        display.add(book);
+        display.clear();
+        display.show("Sucess", "your book is sucessfully added");
+    }
+    else {
+        display.show("danger", "You cannot add this book");
+
+    }
 }
