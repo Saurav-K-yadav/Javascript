6401888715ed01ac7fb51bb3bccf3379aeef9851
Saurav Kumar yadav <sauravkumaryadav100@gmail.com>
2021-09-04 23:33:06 +0545

    try throw and catch

--- Javascript/error.js
+++ Javascript/error.js
@@ -1,1 +1,23 @@
 console.log("Error Handling");
+
+let a = "saurav ";
+// a = undefined;
+try {
+if (a != undefined) {
+    throw new Error("This is not undefined");
+}
+else {
+    console.log("This is undefined");
+}
+
+    function1();
+} catch (error) {
+    console.log('In catch statement');
+    
+    console.log(error);
+
+}
+finally {
+    console.log('This will run finally no matter what');
+
+}
