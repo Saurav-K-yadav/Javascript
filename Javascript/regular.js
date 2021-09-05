599b91c8f3afef6515b27a0f1832d1ef4d35c414
Saurav Kumar yadav <sauravkumaryadav100@gmail.com>
2021-09-06 00:11:19 +0545

    finished the regular expressions

--- Javascript/regular.js
+++ Javascript/regular.js
@@ -8,7 +8,7 @@ console.log(reg.source);
 //Displays value in regular expression literal
 
 //functions to match expresssions:
-let s = "This is a js tutorial and this is supposed to be fun";
+let s = "This is a js tutorial and this is supposed to be a fun";
 
 //1.exec()-> returns array for match and null for no match
 let result = reg.exec(s);
@@ -19,10 +19,20 @@ console.log(result);
 //displays next occurance in case of g flag
 
 //2.test() --> returns true in match else false
-let match = /a/;
+let match = /a/g;
 let out = match.test(s);
 console.log(out);
 
 //3.match() --> It will return array of results or null
 let result2 = s.match(match);//its syntax is opposite of others
+//lists all occurance at once in g flag
 console.log(result2);
+
+//4.search() -->returns index of first match else -1
+let result3 = s.search(match);
+console.log(result3);
+
+//5.replace() -->replaces returns new replaced string with all the replacements
+let result5=s.replace(match,"are");
+// g flag replaces all
+console.log(result5);
